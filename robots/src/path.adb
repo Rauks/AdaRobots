d01460c2eb53cb25d96f784cd9e4db36aa6ce17e
Karl Woditsch <k.woditsch@gmail.com>
2013-10-27 22:39:37 +0100

    Fix path

--- robots/src/path.adb
+++ robots/src/path.adb
@@ -49,7 +49,7 @@ package body Path is
    begin
       StartPoint := Path.Values(Segment);
       EndPoint := path.Values(Segment + 1);
-      Length := Sqrt(abs (EndPoint.X - StartPoint.X) + abs (EndPoint.Y - StartPoint.Y));
+      Length := Sqrt((EndPoint.X - StartPoint.X)**2 + (EndPoint.Y - StartPoint.Y)**2);
       return Length;
    end;
 
@@ -59,7 +59,7 @@ package body Path is
       if Path.Size > 0 then
          TargetPoint := Path.Values(1);
          Adagraph.Goto_XY(Integer(TargetPoint.X), Integer(TargetPoint.Y));
-         for Segment in 2..Segment_Count(Path) loop
+         for Segment in 2..Path.Values'Last loop
             TargetPoint := Path.Values(Segment);
             Adagraph.Draw_To(Integer(TargetPoint.X), Integer(TargetPoint.Y), Color);
          end loop;
