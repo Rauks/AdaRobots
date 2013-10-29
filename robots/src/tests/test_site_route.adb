a19559d9788bc4fa159704e4f8d6d3ace55f541e
Karl Woditsch <k.woditsch@gmail.com>
2013-10-29 17:03:47 +0100

    Test robots routes in test_site_route

--- robots/src/tests/test_site_route.adb
+++ robots/src/tests/test_site_route.adb
@@ -1,22 +1,42 @@
 with Adagraph;
+with Robot;
 with Site;
 
-use Adagraph, Site;
+use Adagraph, Robot, Site;
 
 procedure Test_Site_Route is
+   R1: Robot.Object(Color => Blue);
+   R2: Robot.Object(Color => Red);
+   R3: Robot.Object(Color => Green);
 begin
    Safely.Create_Window;
    Safely.Clear_Window;
    Safely.Draw_Site;
    Safely.Draw_Places;
 
+
    Safely.Draw_Path(P     => Site.Route(From => I3,
                                         To   => O6),
-                    Color => Light_Magenta);
+                    Color => Blue);
    Safely.Draw_Path(P     => Site.Route(From => I2,
                                         To   => O1),
-                    Color => Light_Cyan);
+                    Color => Red);
    Safely.Draw_Path(P     => Site.Route(From => I1,
                                         To   => O5),
-                    Color => Light_Green);
+                    Color => Green);
+
+   delay 5.0;
+   R1.Go(From => I3,
+        To   => O6);
+   delay 1.0;
+   R2.Go(From => I2,
+        To   => O1);
+   delay 1.0;
+   R3.Go(From => I1,
+        To   => O5);
+   delay 1.0;
+
+   R1.Shutdown;
+   R2.Shutdown;
+   R3.Shutdown;
 end;
