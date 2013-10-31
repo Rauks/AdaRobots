179d1a9de5cf34b6288669b6dc43109066fb0505
Karl Woditsch <k.woditsch@gmail.com>
2013-10-31 18:10:27 +0100

    Robots uses safes trajectories

--- robots/src/robot.adb
+++ robots/src/robot.adb
@@ -1,37 +1,37 @@
-with Robot.Trajectory;
+with Robot.Trajectory.Safe;
 
-use Robot.Trajectory;
+use Robot.Trajectory.Safe;
 
 package body Robot is
    task body Object is
       Ready: Boolean := True;
-      RobotRoute: Trajectory.Object;
+      RobotRoute: Trajectory.Safe.Safe_Object;
    begin
       loop
          select
             when Ready =>
                accept Go (From: in Site.Input_Places; To: in Site.Output_Places) do
                   Ready := False;
-                  RobotRoute := Trajectory.Open(From, To, Speed);
+                  RobotRoute := Open(From, To, Speed);
                end;
-               Site.Safely.Draw_Path(P     => Trajectory.Route(RobotRoute),
+               Site.Safely.Draw_Path(P     => Route(RobotRoute),
                                      Color => Color);
-               Site.Safely.Draw_Robot(X     => Integer(Trajectory.X(RobotRoute)),
-                                      Y     => Integer(Trajectory.Y(RobotRoute)),
+               Site.Safely.Draw_Robot(X     => Integer(X(RobotRoute)),
+                                      Y     => Integer(Y(RobotRoute)),
                                       Color => Color);
-               while not Trajectory.At_End(RobotRoute) loop
+               while not At_End(RobotRoute) loop
                   delay Dt;
-                  Site.Safely.Hide_Robot(X     => Integer(Trajectory.X(RobotRoute)),
-                                         Y     => Integer(Trajectory.Y(RobotRoute)));
-                  Trajectory.Next(RobotRoute, Float(Dt));
-                  Site.Safely.Draw_Robot(X     => Integer(Trajectory.X(RobotRoute)),
-                                         Y     => Integer(Trajectory.Y(RobotRoute)),
+                  Site.Safely.Hide_Robot(X     => Integer(X(RobotRoute)),
+                                         Y     => Integer(Y(RobotRoute)));
+                  Next(RobotRoute, Float(Dt));
+                  Site.Safely.Draw_Robot(X     => Integer(X(RobotRoute)),
+                                         Y     => Integer(Y(RobotRoute)),
                                          Color => Color);
                end loop;
                delay Dt;
-               Site.Safely.Hide_Robot(X     => Integer(Trajectory.X(RobotRoute)),
-                                      Y     => Integer(Trajectory.Y(RobotRoute)));
-               Trajectory.Close(RobotRoute);
+               Site.Safely.Hide_Robot(X     => Integer(X(RobotRoute)),
+                                      Y     => Integer(Y(RobotRoute)));
+               Close(RobotRoute);
 
                Ready := True;
          or
