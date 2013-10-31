7939024bd36356676343fdddc2c645cd64be859d
Karl Woditsch <k.woditsch@gmail.com>
2013-10-31 22:36:35 +0100

    Fix Parking for use in Agency

--- robots/src/parking.ads
+++ robots/src/parking.ads
@@ -1,11 +1,14 @@
+with Agency;
 with Robot;
 
-use Robot;
+use Robot, Agency;
 
 package Parking is
+   Size: constant Positive := Agency.Robots'Size;
+
    type Places is array (Positive range <>) of Robot_Id;
 
-   protected type Object(Size: Positive) is
+   protected Object is
       entry Take (Id: out Robot_Id);
       entry Park (Id: in Robot_Id);
    private
@@ -14,6 +17,4 @@ package Parking is
       Last: Positive := Size;
       Count: Natural := 0;
    end Object;
-   type Ref is access Object;
-
 end Parking;
