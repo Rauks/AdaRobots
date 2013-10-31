0a2f2dda6d1e2cc56d74cda04f8972fd5a21cb9c
Karl Woditsch <k.woditsch@gmail.com>
2013-11-01 00:05:58 +0100

    Fix robot mail box size to 10

--- robots/src/robot.ads
+++ robots/src/robot.ads
@@ -1,18 +1,21 @@
-with Adagraph;
 with Mail_Box;
+
+with Adagraph;
 with Path;
 with Site;
 
 use Adagraph, Path, Site;
 
 package Robot is
+   Size_Mail: constant Positive := 10;
+
    Speed: constant Float := 75.0;
    Dt: constant Duration := 0.05;
 
    type Robot_Id is new Positive;
    package Robot_Mail is new Mail_Box(Robot_Id);
 
-   Mail: Robot_Mail.Ref := new Robot_Mail.Object(6);
+   Mail: Robot_Mail.Ref := new Robot_Mail.Object(Size_Mail);
 
    task type Object (Id: Robot_Id; Mail: Robot_Mail.Ref; Color: Color_Type) is
       entry Go (From: in Site.Input_Places; To: in Site.Output_Places);
