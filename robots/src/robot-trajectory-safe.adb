d13eaba4140c1dbbfae3de7611340cfa5cb0e9fb
Karl Woditsch <k.woditsch@gmail.com>
2013-10-31 16:44:17 +0100

    Add safe trajectories using places reservations

---
+++ robots/src/robot-trajectory-safe.adb
@@ -0,0 +1,49 @@
+with Site.Places_Path;
+
+use Site.Places_Path;
+
+package body Robot.Trajectory.Safe is
+   function Open (From: in Site.Input_Places; To: in Site.Output_Places; Speed: in float) return Safe_Object is
+      Trajectory: Object;
+      Trajectory_Safe: Safe_Object;
+      Request: Resources_Pool.Request_Map;
+      P: Site.Places_Path.Object;
+   begin
+      P := Places_Path.Open(From, To);
+      while not Places_Path.At_End(P) loop
+         Request(Places_Path.Value(P)) := True;
+         Places_Path.Next(P);
+      end loop;
+
+      Resources_Pool.Acquire(Map => Request);
+
+      Places_Path.Reset(P);
+
+      Trajectory := Open(From, To, Speed);
+      Trajectory_Safe := (Trajectory with Places => P);
+
+      return Trajectory_Safe;
+   end;
+
+   procedure Next (Trajectory: in out Safe_Object; dt: in Float) is
+      In_Place, In_Place_Again: Boolean;
+      Was_X: Float := Trajectory.X;
+      Was_Y: Float := Trajectory.Y;
+   begin
+      Next(Object(Trajectory), dt);
+
+      In_Place := (for some Place in Place_Names'Range => Site.Robot_Intersect(Place, Was_X, Was_Y));
+      In_Place_Again := (for some Place in Place_Names'Range => Site.Robot_Intersect(Place, Trajectory.X, Trajectory.Y));
+      if In_Place and not In_Place_Again then
+         Resources_Pool.Release(Places_Path.Value(Trajectory.Places));
+         Places_Path.Next(Trajectory.Places);
+      end if;
+   end;
+
+   procedure Close (Trajectory: in out Safe_Object) is
+   begin
+      Resources_Pool.Release(Places_Path.Value(Trajectory.Places));
+      Places_Path.Close(Trajectory.Places);
+   end;
+
+end Robot.Trajectory.Safe;
---
+++ robots/src/robot-trajectory-safe.ads
@@ -0,0 +1,20 @@
+with Generic_Resources_Pool;
+
+with Site;
+with Site.Places_Path;
+
+use Site, Site.Places_Path;
+
+private package Robot.Trajectory.Safe is
+   type Safe_Object is new Object with private;
+
+   overriding function Open (From: in Site.Input_Places; To: in Site.Output_Places; Speed: in float) return Safe_Object;
+   overriding procedure Next (Trajectory: in out Safe_Object; dt: in Float);
+   overriding procedure Close (Trajectory: in out Safe_Object);
+private
+   type Safe_Object is new Object with record
+      Places: Places_Path.Object;
+   end record;
+
+   package Resources_Pool is new Generic_Resources_Pool(Site.Place_Names);
+end Robot.Trajectory.Safe;
