aecce535542180a5938de8e8ed7bb3595e9da7a0
Karl Woditsch <k.woditsch@gmail.com>
2013-11-01 20:29:41 +0100

    Test shutdow #2

---
+++ robots/src/tests/test_shutdown.adb
@@ -0,0 +1,29 @@
+with Ada.Numerics.Discrete_Random;
+
+with Agency;
+with Site;
+
+use Agency, Site;
+
+procedure Test_Shutdown is
+   package Mission_Inputs is new Ada.Numerics.Discrete_Random(Site.Input_Places);
+   package Mission_Outputs is new Ada.Numerics.Discrete_Random(Site.Output_Places);
+
+   Input_Generator: Mission_Inputs.Generator;
+   Output_Generator: Mission_Outputs.Generator;
+begin
+   Safely.Create_Window;
+   Safely.Clear_Window;
+   Safely.Draw_Site;
+   Safely.Draw_Parking;
+
+   Mission_Inputs.Reset(Input_Generator, 345);
+   Mission_Outputs.Reset(Output_Generator, 543);
+
+   for I in 1..20 loop
+      Agency.Handle_Transfert(From => Mission_Inputs.Random(Input_Generator),
+                              To   => Mission_Outputs.Random(Output_Generator));
+   end loop;
+
+   Agency.Shutdown;
+end;
